ab7e1cde81f498377a77c3f6c374a1db8aa717d7
Jay Wineinger <jay.wineinger@gmail.com>
2011-07-28 18:39:47 +0000

    Added bar plot of daily weight changes

--- plot.py
+++ plot.py
@@ -2,14 +2,16 @@ from decimal import Decimal
 import matplotlib, os.path, csv
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-path = os.path.dirname(os.path.abspath(__file__))
+import matplotlib.mlab as mlab
+
+plt.subplot(2,1,1)
 
 reader = csv.reader(open('chart_weights.txt'))
 ages, weights = [], []
 last_date = None
 for line in reader:
     ages.append(32. + (2 + int(line[1])) / 7.)
-    weights.append(int(line[2]) / 1000.)
+    weights.append(int(line[2]))
     last_date = line[0]
 
 pciles = {}
@@ -21,22 +23,48 @@ for line in reader:
     week = int(line[0])
     val = Decimal(line[2])
     if pcile not in pciles:
-        pciles[pcile] = {week:val}
-    pciles[pcile][week] = val
+        pciles[pcile] = {week: val * 1000}
+    pciles[pcile][week] = val * 1000
 
 for pcile, data in sorted(pciles.iteritems()):
     plt.plot(data.keys(), data.values())
 
 plt.plot(ages, weights, 'mo-')
-plt.ylabel("Weight (kg)")
+plt.ylabel("Weight (g)")
 plt.xlabel("Week")
-plt.axis([30,40,.5,4.0])
+plt.axis([30,40,500,5000])
 plt.grid(True)
 plt.legend(('3%','10%','50%','90%','97%','Kate'), loc=2, fancybox=True, shadow=True, ncol=2)
 plt.annotate(
-    '%d g on %s' % (weights[-1] * 1000, last_date),
-    xy=(ages[-1] + .1, weights[-1] - .05),
-    xytext=(ages[-1] + .5, weights[-1] - .3),
-    arrowprops=dict(facecolor='black', shrink=0),
+    '%d g' % weights[-1],
+    xy=(ages[-1], weights[-1]),
+    xycoords='data',
+    xytext=(20, -50),
+    textcoords='offset points',
+    arrowprops=dict(arrowstyle="->",
+                    connectionstyle="angle,angleA=0,angleB=90,rad=10"),
 )
-plt.savefig(os.path.join(path, "www/fenton.png"))
+
+# start the second plot
+plt.subplot(2,1,2)
+plt.ylabel("Weight Change (g)")
+plt.axis([30,40,-30,130])
+plt.grid(True)
+
+weight_changes, bar_colors = [], []
+i = 0
+# calculate daily weight changes and set colors for positive/negative
+while i <= (len(weights) - 2):
+    weight_changes.append(weights[i+1] - weights[i])
+    if weight_changes[-1] > 0:
+        bar_colors.append('b')
+    else:
+        bar_colors.append('r')
+    i += 1
+plt.bar(ages[1:], weight_changes, width=1/7., color=bar_colors, align='center')
+
+fig = plt.gcf()
+fig.set_size_inches(8,10)
+
+path = os.path.dirname(os.path.abspath(__file__))
+plt.savefig(os.path.join(path, "www/fenton.png"), dpi=100)
